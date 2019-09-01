13e58e1d376635c86f9110e1e886283bfa4ca7f5
swapneal <52267392+swapneal@users.noreply.github.com>
2019-08-31 21:21:50 -0700

    code optimization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,13 @@ import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { HttpClientModule } from "@angular/common/http";
 import { GroceryComponent } from "./grocery/grocery.component";
 
 import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [AppComponent, GroceryComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
+  imports: [BrowserModule, AppRoutingModule,  FormsModule],
   providers: [],
   bootstrap: [AppComponent, GroceryComponent]
 })
